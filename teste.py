0538e3d89a2e3136d706151872957fa749c8fc39
Mariaquiteria26 <alan.cavalcante4@aluno.ce.gov.br>
2025-08-31 17:25:51 -0300

    lista laços de repetição

---
+++ teste.py
@@ -0,0 +1,50 @@
+#for i in range(11):
+#    print(i)
+
+#x = 10  
+#while x >= 0:
+#    print(x)  
+#    x -= 1
+
+#nome = input("Qual seu nome?")
+#for i in range (5):
+#    print(nome)
+
+#for i in range (0,21, 2):
+#    print(i)
+
+#senha_correta = "1234"
+#while True:
+#    senha = input("Digite sua senha:").strip()
+#    if senha == senha_correta:
+#        print("acesso liberado")
+#        break
+#    else:
+#        print("senha incorreta, tente novamente.")
+
+#N = 0
+#for i in range (5):
+#    n = float(input(f"Digite o {i+1}° número: "))
+#    N += n
+#    print(f"A soma é {N}")]
+
+#for i in range (0, 11):
+#    if i % 2 != 0:
+#        continue
+#    print(i)
+
+#cont = 0
+#while True:
+#  user = input("Digite o número: ").strip()
+#  if user == "0":
+#    print(cont)
+#    break
+#  cont +=1
+
+#numero = int(input('Digite o número:'))
+#for i in range (0, 11):
+#    numero * i
+#    print(f"O número {numero} x {i} = {numero*i}")
+
+
+
