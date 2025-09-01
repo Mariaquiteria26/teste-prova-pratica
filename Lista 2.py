51eb4ad8988e6a4d6f8a740fe3d4ff902a2650ab
Mariaquiteria26 <alan.cavalcante4@aluno.ce.gov.br>
2025-08-31 21:12:17 -0300

    Lista 2, condicionais

---
+++ Lista 2.py
@@ -0,0 +1,55 @@
+#soma = 0
+#for i in range (3):
+#    notas = float(input(f"Digite a {i+1}° nota: "))
+#    soma += notas
+#med = soma / 3
+#if med >= 7:
+#    print(f"{med} Você foi aprovado, parabéns!")
+#else:
+#    print(f"{med} Você foi reprovado!")
+
+#cont = 0
+#vogal = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
+#palavra = input("Digite uma palavra: ")
+#for letra in palavra:
+#    if letra in vogal:
+#        cont +=1
+#print(f"A palavra tem {cont} vogais")
+
+
+#n = int(input("Digite um número: "))
+#if n % 2 == 0:
+#    print("O número é par!")
+#else:
+#    print("O número é impar!")
+
+
+#for i in range (5):
+#    N = int(input(f"Digite {i+1}° número: "))
+#    if i == 0:
+#      maior = menor = N
+#    elif N > maior:
+#      maior = N    
+#    elif N < menor:
+#        menor = N
+#print(f"O maior é {maior} e o menor é {menor}!")
+
+#cont = total = 0
+#while True:
+#    N = int(input("Digite os números: "))
+#    if N == 0:
+#        break
+#    total += N
+#    if N >= 100:
+#        cont += 1
+#print(f"A soma é {total}")
+#print(f"{cont} números são maiores que 100")
+
+
+
+
+
+  
+
+
+
