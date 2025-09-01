f9a65179a15c1e0007ff602009dd2937ed9edfe1
Mariaquiteria26 <alan.cavalcante4@aluno.ce.gov.br>
2025-08-31 23:04:59 -0300

    Lista 3 não terminada

---
+++ Lista 3.py
@@ -0,0 +1,32 @@
+#altura = float(input("Digite sua altura: "))
+#peso = float(input("Digite seu peso: "))
+#imc = peso / altura **2
+#if imc <= 18.5:
+#    print(f"Você esta abaixo do peso")
+#elif imc <= 24.9: 
+#    print(f"Seu peso está normal")
+#elif imc <= 29.9:
+#    print(f"sobrepeso")
+#else:
+#    imc > 30
+#    print("obesidade")
+
+
+#N = int(input("Digite um número: "))
+#for i in range(N + 1):
+#    if i % 2 == 0:
+#        print("O número é par")
+#    else:
+#        print("O número é impar")
+
+
+#N = int(input("Digite um número: "))
+#for i in range(1,11):
+#    resultado = i * N
+#    print(f"{N} * {i} = {resultado} ")
+
+
+
+
+
+
